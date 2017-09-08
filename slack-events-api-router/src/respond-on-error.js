0268292f245fe5f46119fb24384af1b5a529070f
chriszarate <chris@zarate.org>
2017-09-08 13:00:30 -0400

    Mark response bot as @todo.

--- slack-events-api-router/src/respond-on-error.js
+++ slack-events-api-router/src/respond-on-error.js
@@ -5,7 +5,12 @@ function respondOnError(error) {
     message: error.message,
   };
 
-  return invokeLambdaFunction(payload, 'need-to-write-this');
+  console.error(`Responding to user with error: ${error.message}`);
+
+  // @todo Write the Lambda function that responds if there is an error.
+  // return invokeLambdaFunction(payload, 'need-to-write-this');
+
+  return Promise.resolve('OK');
 }
 
 module.exports = respondOnError;
