aa303cbe58ec89bff534da467093e8b8df8a1039
John Keefe <john@johnkeefe.net>
2017-09-27 17:33:04 -0400

    added databot to commands.js

--- slack-events-api-message-handler/commands.js
+++ slack-events-api-message-handler/commands.js
@@ -3,6 +3,12 @@ module.exports = {
         type: 'lambda',
         functionName: 'quackbot-screenshot',
         usage: 'screenshot www.example.com',
-        description: 'Grabs a screenshot of a website and Slacks it at you.'
+        description: 'Grabs a screenshot of a website and Slacks it at you.',
+    },
+    data: {
+        type: 'lambda',
+        functionName: 'quack-search-sheet',
+        usage: 'data agriculture',
+        description: 'Searches Christopher Groskopf\'s spreadsheet of good data sources.',
     },
 };
