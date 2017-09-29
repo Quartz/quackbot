4592855e5c61036dd5e675ee06ad1ee50b593a47
Ted Han <ted@knowtheory.net>
2017-09-29 07:30:17 -0700

    Need to add foreign key to authorizations to make Sequelize play nice.

---
+++ oauth-dancer/lib/migrations/20170928232847-add_team_id_to_authorizations.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.addColumn('authorizations', 'team_id', Sequelize.INTEGER, {
+      allowNull: false
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.removeColumn('authorizations', 'team_id');
+  }
+};
