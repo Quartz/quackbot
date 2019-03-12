c3ebd44842d6a1a3dd6722a9e1bcb0d04571a909
John Keefe <john@johnkeefe.net>
2019-03-12 19:01:03 -0400

    more wip

--- slack-events-api-bots/sheets_json/file-checker-api/index.js
+++ slack-events-api-bots/sheets_json/file-checker-api/index.js
@@ -29,7 +29,7 @@ module.exports = function(incoming) {
   
     return new Promise(function(resolve, reject){
         
-        checkS3(incoming)
+        mainFunction(incoming)
             // .then(buildSlackResponse)   
             .then( (results) => {
                 console.log("Main code:", results);
@@ -44,7 +44,7 @@ module.exports = function(incoming) {
     
 };
 
-async function checkS3(payload) {
+async function mainFunction(payload) {
 
     // the directory is the team identifier (TODO: Encrypt This)
     var directory = payload.sessionId.match(/^(.*)-/)[1];
@@ -55,32 +55,8 @@ async function checkS3(payload) {
     };
         
     let file_list = await listS3(params);
-    
-    if (!file_list || file_list.length < 2) {
-        return "this will be the give me a URL statement list";
-    }
-    
-    let file_promises = []
-    
-    // build a promise list to get all the urls
-    for (i=0; file_list.length; i++) {
-        
-        // skip the directory key
-        if (file_list[i] == `${directory}/jsons/`) continue;
-        
-        // turn 'T111111Z/jsons/My%20example%20spreadsheet.json'
-        // into 'T111111Z/urls/My%20example%20spreadsheet.txt'
-        var file_to_read = file_list[i].replace("/jsons/","/urls/").replace(/\.json$/, ".txt");
-        
-        var params = {
-            Bucket: s3_bucket;
-            Key: file_to_read;
-        }
-        
-        /// STOPPED HERE ... NEED MORE S3 READ code
-        
-    }
-    
+    let all_the_links = await getAllTheFiles(file_list);
+    let slack_response = await buildSlackResponse(all_the_links);
     
     
     return file_list;
@@ -96,6 +72,48 @@ function listS3(params) {
 }
 
 function readS3(params) {
+    // read the URL off S3 and return:
+    // {
+    //    "name": url-decoded sheet name,
+    //    "url": string of the spreadsheet URL
+    // }
     
+}
+
+async function getAllTheFiles(file_list) {
+    
+    if (!file_list || file_list.length < 2) {
+        return "this will be the give me a URL statement list";
+    }
+    
+    let file_promises = [];
+    
+    // build a promise list to get all the urls
+    for (var i=0; file_list.length; i++) {
+        
+        // skip the directory key 'T111111Z/jsons/'
+        if (file_list[i].Size == 0) continue;
+        
+        // turn 'T111111Z/jsons/My%20example%20spreadsheet.json'
+        // into 'T111111Z/urls/My%20example%20spreadsheet.txt'
+        var file_to_read = file_list[i].Key.replace("/jsons/","/urls/").replace(/\.json$/, ".txt");
+        
+        var params = {
+            Bucket: s3_bucket,
+            Key: file_to_read            /// STOPPED HERE ... NEED MORE S3 READ code
+        };
+        
+        file_promises.push(readS3(params));
+        
+    }
+
+    let results = await Promise.all(file_promises);
+    return results;
+    
+}
+
+function buildSlackResponse(file_links) {
+    
+    // build the block with the array of file infos
     
 }
