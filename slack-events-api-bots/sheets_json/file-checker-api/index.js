2113ee14bedf2ac62bea4c5854f7223debbe523f
John Keefe <john@johnkeefe.net>
2019-03-14 21:24:01 -0400

    got file-checker api working

--- slack-events-api-bots/sheets_json/file-checker-api/index.js
+++ slack-events-api-bots/sheets_json/file-checker-api/index.js
@@ -1,5 +1,4 @@
 // require modules here
-var slack_test = require('./samples/slack_block.json');
 const AWS = require('aws-sdk');
 AWS.config.apiVersions = {
     s3: '2006-03-01'
@@ -15,6 +14,10 @@ module.exports = function(incoming) {
     // incoming is from dialogflow api V1  TODO: Upgrade to V2
     // see: https://dialogflow.com/docs/fulfillment/how-it-works
   
+    //// TEMPORARY DEV THING. 
+    // Setting sessin id here to test from Dialogflow. Delete this in production.
+    incoming.sessionId = "T111111Z-U222222W";
+  
     // establish a blank return object
     var sendback = {
         "speech": "",
@@ -26,18 +29,22 @@ module.exports = function(incoming) {
         "source": "Quackbot File-Checker API",
         "followupEvent": {}
     };
-  
+        
     return new Promise(function(resolve, reject){
         
         mainFunction(incoming)
             // .then(buildSlackResponse)   
             .then( (results) => {
-                console.log("Main code:", results);
+                console.log("Main code:", JSON.stringify(results));
                 
                 // decode the team id from the sessionId
                 sendback.speech = "This is the result from the API! Congrats!";
-                sendback.data.slack = slack_test;
+                sendback.data.slack = results;
                 resolve(sendback);
+            })
+            .catch( (err) => {
+                console.log("I caught a main function error:", err);
+                reject(err);
             });
         
     });
@@ -45,6 +52,8 @@ module.exports = function(incoming) {
 };
 
 async function mainFunction(payload) {
+
+    console.log("ID:", payload.sessionId);
 
     // the directory is the team identifier (TODO: Encrypt This)
     var directory = payload.sessionId.match(/^(.*)-/)[1];
@@ -57,9 +66,8 @@ async function mainFunction(payload) {
     let file_list = await listS3(params);
     let all_the_links = await getAllTheFiles(file_list);
     let slack_response = await buildSlackResponse(all_the_links);
-    
-    
-    return file_list;
+
+    return slack_response;
 }
 
 function listS3(params) {
@@ -71,25 +79,19 @@ function listS3(params) {
     });
 }
 
-function readS3(params) {
-    // read the URL off S3 and return:
-    // {
-    //    "name": url-decoded sheet name,
-    //    "url": string of the spreadsheet URL
-    // }
-    
-}
+
 
 async function getAllTheFiles(file_list) {
     
+    // return null if there are no files to list
     if (!file_list || file_list.length < 2) {
-        return "this will be the give me a URL statement list";
+        return null;
     }
     
     let file_promises = [];
     
     // build a promise list to get all the urls
-    for (var i=0; file_list.length; i++) {
+    for (var i=0; i < file_list.length; i++) {
         
         // skip the directory key 'T111111Z/jsons/'
         if (file_list[i].Size == 0) continue;
@@ -100,7 +102,7 @@ async function getAllTheFiles(file_list) {
         
         var params = {
             Bucket: s3_bucket,
-            Key: file_to_read            /// STOPPED HERE ... NEED MORE S3 READ code
+            Key: file_to_read
         };
         
         file_promises.push(readS3(params));
@@ -112,8 +114,106 @@ async function getAllTheFiles(file_list) {
     
 }
 
+function readS3(params) {
+    
+    return new Promise ((resolve, reject) => {
+        
+        // read the URL off S3 and return:
+        // {
+        //    "name": url-decoded sheet name,
+        //    "sheet_url": string of the spreadsheet URL
+        // }
+        
+        // extract the clean file name from the whole, url-encoded one
+        var file_name = decodeURI(params.Key).match(/.*\/urls\/(.*).txt/)[1];
+        
+        var file_object = {
+            "name": file_name
+        };
+        
+        s3.getObject(params, (err, data) => {
+
+            try {
+                file_object.sheet_url = data.Body.toString('utf-8');              
+                resolve(file_object);
+                return;
+            } catch (err) {
+                console.log("Error getting S3 file:", params.Key, err);
+                reject(err);
+                return;
+            }
+        });
+        
+    });
+    
+    
+}
+
 function buildSlackResponse(file_links) {
     
-    // build the block with the array of file infos
+    // establish a blank slack block object
+    var slack_back = [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "Looks like you have existing JSON files I could update for you. Cool!"
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "To *update* an existing file, pick one from the list here ..."
+            },
+            "accessory": {
+                "action_id": "quackbot-gsheet-to-json",
+                "type": "static_select",
+                "placeholder": {
+                    "type": "plain_text",
+                    "text": "Pick a file ...",
+                    "emoji": true
+                },
+                "options": []
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "To make a *new* JSON file ... \n1. Share the new Google spreadsheet with quackbot-tbd@gmail.com\n2. Give me the sharing link for that new spreadsheet. "
+            }
+        }
+    ];
+    
+    // just return the last instruction if there are no file links
+    if (!file_links) {
+        return slack_back[2];
+    }
+    
+    var option_list = [];
+    
+    // build the options list
+    file_links.forEach( (file) => {
+        
+        option_list.push(
+            {
+                "text": {
+                    "type": "plain_text",
+                    "text": file.name,
+                    "emoji": true
+                },
+                "value": file.sheet_url
+            }
+        );
+        
+    });
+    
+    // insert the options list into the blank slack_back object
+    // where it goes (which, for us, is inside the second element in the array)
+
+    slack_back[1].accessory.options = option_list;
+    return slack_back;
+    
     
 }
